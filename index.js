00f73c51755056f0cca85d18249b4006abccb62b
Weverson Meneses <weversonmnzs@gmail.com>
2023-05-14 15:52:09 -0300

    Update configs

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
+const res = require("express/lib/response");
 const server = express();
-const filmes = require('./src/data/filmes.json');
+const filmes = require("./src/data/filmes.json");
 
-server.get('/filmes', (req,res) => {
-  return res.json({filmes})
+server.get("/filmes", (req, res) => {
+	return res.json({ filmes });
 });
 
 server.listen(3000, () => {
-  console.log('Servidor está funcionando')
+	console.log("Servidor está funcionando");
 });
